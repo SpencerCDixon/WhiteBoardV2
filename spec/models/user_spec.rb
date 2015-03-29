acfe127485bb3e066d90a3787392cb1d92f7cfd2
Spencer Dixon <spencercdixon@gmail.com>
2015-03-28 20:02:29 -0400

    Added tests for user methods

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,4 +14,16 @@ RSpec.describe User, type: :model do
       expect(user.has_family?).to eq false
     end
   end
+
+  describe '#email_verified?' do
+    it 'returns false when there is a default email ' do
+      user = FactoryGirl.create(:user, email: 'change@me-slkjdf.com')
+      expect(user.email_verified?).to eq false
+    end
+
+    it 'returns true when there is a good email' do
+      user = FactoryGirl.create(:user)
+      expect(user.email_verified?).to eq true
+    end
+  end
 end
