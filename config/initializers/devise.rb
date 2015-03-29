9021f99cd859a4885a17d0fdc634797a7d3be0e9
Spencer Dixon <spencercdixon@gmail.com>
2015-03-28 21:23:15 -0400

    Pre compiled assets before pushing to heroku

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -11,6 +11,7 @@ Devise.setup do |config|
   config.password_length = 8..128
   config.reset_password_within = 6.hours
   config.sign_out_via = :delete
+  config.secret_key = '5393089661910e154e9a0c2f10bf9e9ae44b62ce45bb63b8c02c46710ec1b9204ac0514010d653ea861e5a9b460359ca718cc8943f2fa0395dfb9b62ac872800'
 
   config.omniauth :twitter, ENV["TWITTER_CLIENT_ID"], ENV["TWITTER_CLIENT_SECRET"]
   config.omniauth :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"]
