86dacebf8d765a43ed18596efb64d9b742b1d1ef
Spencer Dixon <spencercdixon@gmail.com>
2015-04-19 16:08:19 -0400

    Started writing conversation builder tests but realized it would make sense to wait until there is more complicated logic

---
+++ spec/models/conversation_builder_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+# these should be tested at some point, gonna wait until this logic ends up
+# changing and becoming more difficult
+describe ConversationBuilder do
+  describe "#send_message" do
+
+  end
+
+  describe "#create_conversation" do
+
+  end
+
+  describe "#create_participants" do
+
+  end
+end
